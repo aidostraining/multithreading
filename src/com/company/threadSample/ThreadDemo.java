4b6e7b3de7ff4921acb13dbd5abb405a7e5c10d2
Aidos <aidoz@mail.ru>
2020-03-07 11:54:10 +0600

    Multithreading examples from the lecture

---
+++ src/com/company/threadSample/ThreadDemo.java
@@ -0,0 +1,13 @@
+package com.company.threadSample;
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        Talk talk  = new Talk();
+        Thread walk = new Thread(new Walk());
+        talk.start();
+        walk.start();
+
+        //Walk w = new Walk(); // просто объект, не поток
+        // w.run(); //выполнится метод, но поток не запустится!
+    }
+}
